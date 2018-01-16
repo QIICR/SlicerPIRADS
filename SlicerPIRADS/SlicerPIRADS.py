cc1dd2d1960bda2e87c6833a0e97042b760b65d6
Christian Herz <christianherz1985@googlemail.com>
2018-01-16 15:59:22 -0500

    ENH: added initial code for module and slicelet

---
+++ SlicerPIRADS/SlicerPIRADS.py
@@ -0,0 +1,118 @@
+import slicer
+import os
+import qt
+from slicer.ScriptedLoadableModule import *
+
+from SlicerDevelopmentToolboxUtils.mixins import ModuleWidgetMixin, ModuleLogicMixin
+
+
+class SlicerPIRADS(ScriptedLoadableModule):
+  """Uses ScriptedLoadableModule base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def __init__(self, parent):
+    ScriptedLoadableModule.__init__(self, parent)
+    self.parent.title = "Slicer PIRADS"
+    self.parent.categories = ["Informatics", "Quantification", "Segmentation"]
+    self.parent.dependencies = ["SlicerDevelopmentToolbox"]
+    self.parent.contributors = ["Christian Herz (SPL, BWH)", "Andrey Fedorov (SPL, BWH)"]
+    self.parent.helpText = """
+      
+    """
+    self.parent.acknowledgementText = """ 
+      Surgical Planning Laboratory, Brigham and Women's Hospital, Harvard 
+      Medical School, Boston, USA This work was supported in part by the 
+      National Institutes of Health through grants U24 CA180918,
+      R01 CA111288 and P41 EB015898.
+    """
+
+
+class SlicerPIRADSWidget(ModuleWidgetMixin, ScriptedLoadableModuleWidget):
+
+  def __init__(self, parent=None):
+    ScriptedLoadableModuleWidget.__init__(self, parent)
+    self.modulePath = os.path.dirname(slicer.util.modulePath(self.moduleName))
+
+  def setup(self):
+    ScriptedLoadableModuleWidget.setup(self)
+
+
+class SlicerPIRADSLogic(ScriptedLoadableModuleLogic):
+
+  def __init__(self):
+    ScriptedLoadableModuleLogic.__init__(self)
+
+
+class SlicerPIRADSSlicelet(qt.QWidget, ModuleWidgetMixin):
+
+  def __init__(self):
+    qt.QWidget.__init__(self)
+    self.mainWidget = qt.QWidget()
+    self.mainWidget.objectName = "qSlicerAppMainWindow"
+    self.mainWidget.setLayout(qt.QHBoxLayout())
+
+    self.setupLayoutWidget()
+
+    self.moduleFrame = qt.QWidget()
+    self.moduleFrame.setLayout(qt.QVBoxLayout())
+    self.widget = SlicerPIRADSWidget(self.moduleFrame)
+    self.widget.setup()
+
+    self.scrollArea = qt.QScrollArea()
+    self.scrollArea.setWidget(self.widget.parent)
+    self.scrollArea.setWidgetResizable(True)
+    self.scrollArea.setMinimumWidth(self.widget.parent.minimumSizeHint.width())
+
+    self.splitter = qt.QSplitter()
+    self.splitter.setOrientation(qt.Qt.Horizontal)
+    self.splitter.addWidget(self.scrollArea)
+    self.splitter.addWidget(self.layoutWidget)
+    self.splitter.splitterMoved.connect(self.onSplitterMoved)
+
+    self.splitter.setStretchFactor(0, 0)
+    self.splitter.setStretchFactor(1, 1)
+    self.splitter.handle(1).installEventFilter(self)
+
+    self.mainWidget.layout().addWidget(self.splitter)
+    self.mainWidget.show()
+
+  def setupLayoutWidget(self):
+    # TODO: configurable ...
+    self.layoutWidget = qt.QWidget()
+    self.layoutWidget.setLayout(qt.QHBoxLayout())
+    layoutWidget = slicer.qMRMLLayoutWidget()
+    layoutManager = slicer.qSlicerLayoutManager()
+    layoutManager.setMRMLScene(slicer.mrmlScene)
+    layoutManager.setScriptedDisplayableManagerDirectory(slicer.app.slicerHome + "/bin/Python/mrmlDisplayableManager")
+    layoutWidget.setLayoutManager(layoutManager)
+    slicer.app.setLayoutManager(layoutManager)
+    layoutWidget.setLayout(slicer.vtkMRMLLayoutNode.SlicerLayoutFourUpView)
+    self.layoutWidget.layout().addWidget(layoutWidget)
+
+  def eventFilter(self, obj, event):
+    if event.type() == qt.QEvent.MouseButtonDblClick:
+      self.onSplitterClick()
+
+  def onSplitterMoved(self, pos, index):
+    vScroll = self.scrollArea.verticalScrollBar()
+    print self.moduleFrame.width, self.widget.parent.width, self.scrollArea.width, vScroll.width
+    vScrollbarWidth = 4 if not vScroll.isVisible() else vScroll.width + 4
+    if self.scrollArea.minimumWidth != self.widget.parent.minimumSizeHint.width() + vScrollbarWidth:
+      self.scrollArea.setMinimumWidth(self.widget.parent.minimumSizeHint.width() + vScrollbarWidth)
+
+  def onSplitterClick(self):
+    if self.splitter.sizes()[0] > 0:
+      self.splitter.setSizes([0, self.splitter.sizes()[1]])
+    else:
+      minimumWidth = self.widget.parent.minimumSizeHint.width()
+      self.splitter.setSizes([minimumWidth, self.splitter.sizes()[1] - minimumWidth])
+
+
+if __name__ == "SlicerPIRADSSlicelet":
+  import sys
+
+  print(sys.argv)
+
+  slicelet = SlicerPIRADSSlicelet()
+
